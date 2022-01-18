72ad815523bb5f6ac5bd2d00fb1ac7781602237f
unknown <juoznark@gmail.com>
2022-01-18 16:17:14 +0200

    Frontend: Added base login form and user program list

---
+++ frontend/src/constants/Routes.js
@@ -0,0 +1,11 @@
+const ROUTES = {
+  HOME: '/',
+  SPORT: '/sport',
+  MY_PROGRAMS: '/sport/myPrograms',
+  NUTRITION: '/nutrition',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  CREATE_PROGRAM: '/sport/create',
+};
+
+export default ROUTES;
