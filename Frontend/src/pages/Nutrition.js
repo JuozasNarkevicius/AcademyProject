feef9a340b6865a497cbde54f2c46ed6f198cdf5
Juonar <74144051+JuozasNarkevicius@users.noreply.github.com>
2022-01-18 16:27:55 +0200

    Delete Frontend directory

--- Frontend/src/pages/Nutrition.js
+++
@@ -1,5 +0,0 @@
-const Nutrition = () => (
-  <h1>This is the nutrition page!</h1>
-);
-
-export default Nutrition;
--- Frontend/src/pages/Register.js
+++
@@ -1,5 +0,0 @@
-const Register = () => (
-  <h1>This is the registration page!</h1>
-);
-
-export default Register;
