5f1e4351eca21120563a3fcddb8173f560c7fa47
unknown <juoznark@gmail.com>
2022-01-23 19:35:05 +0200

    Backend: deleted unnecessary comments in Program.cs

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -2,10 +2,7 @@ using Application.DatabaseContext;
 using Application.Jwt;
 using Application.Repositories;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddCors();
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
     builder
@@ -39,41 +35,11 @@ builder.Services.AddScoped<IWorkoutProgramRepository, WorkoutProgramRepository>(
 builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-//builder.Services.AddAuthentication(x =>
-//{
-//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//}).AddJwtBearer(x =>
-//{
-//    x.RequireHttpsMetadata = false;
-//    x.SaveToken = true;
-//    x.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuerSigningKey = true,
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["AppSettings:Secret"])),
-//        ValidateIssuer = false,
-//        ValidateAudience = false,
-//        ClockSkew = TimeSpan.Zero
-//    };
-//});
-
-
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//    .AddCookie(options =>
-//    {
-//        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-//        options.SlidingExpiration = true;
-//        options.AccessDeniedPath = "/Forbidden/";
-//    });
-
 builder.Services.AddAuthentication(options => {
     options.DefaultScheme = "Cookies";
 }).AddCookie("Cookies", options => {
     options.Cookie.Name = "accessToken";
     options.Cookie.SameSite = SameSiteMode.None;
-    //options.Cookie.Expiration = TimeSpan.FromMinutes(1);
-    //options.ExpireTimeSpan = new TimeSpan(0, 0, 0, 20);
-    //options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
     options.Cookie.MaxAge = TimeSpan.FromHours(5);
     options.Events = new CookieAuthenticationEvents
     {
@@ -92,16 +58,6 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-//app.UseCors(x => x
-//        .AllowAnyOrigin()
-//        .AllowAnyMethod()
-//        .AllowAnyHeader());
-
-//app.UseCors(corsPolicyBuilder =>
-//   corsPolicyBuilder.WithOrigins("http://localhost:3000/")
-//  .AllowAnyMethod()
-//  .AllowAnyHeader()
-//);
 app.UseCors("corsapp");
 
 app.UseHttpsRedirection();
