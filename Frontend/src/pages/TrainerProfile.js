cac1ffd7ef669a763a203ad76c8ae37401a74fcf
JuozasNarkevicius <juoznark@gmail.com>
2022-04-13 22:06:42 +0300

    Frontend: added design to trainer profile

--- Frontend/src/pages/TrainerProfile.js
+++ Frontend/src/pages/TrainerProfile.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Card, Container, CardContent, Typography,
+  Card, Container, CardContent, Typography, CssBaseline, Box, Grid, ButtonBase,
 } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import applicationService from '../services/ApplicationService';
 import firebaseStorage from '../services/FirebaseStorage';
 import Loading from '../components/Loading';
+import COLORS from '../styles/colors';
+import backgroundImage from '../assets/images/workoutEquipment.jpg';
 
 const TrainerProfile = () => {
   const [trainer, setTrainer] = useState();
@@ -29,27 +31,67 @@ const TrainerProfile = () => {
   }
 
   return (
-    <Container sx={{ mt: '2rem' }}>
-      <Card sx={{ minHeight: '30rem' }}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Trainer profile
-          </Typography>
-          <img src={trainer.profileImage} alt="" />
-          <Typography variant="body2" color="text.secondary">
-            {`${trainer.firstName}${' '}${trainer.lastName}`}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {trainer.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {trainer.qualification}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {trainer.phoneNumber}
-          </Typography>
-        </CardContent>
-      </Card>
+    <Container sx={{
+      minWidth: '100%',
+      minHeight: '93vh',
+      paddingTop: '2rem',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      overflow: 'hidden',
+    }}
+    >
+      <CssBaseline />
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Card sx={{ minHeight: '30rem', width: '70rem', backgroundColor: COLORS.ITEM }}>
+          <CardContent>
+            <Grid container spacing={4}>
+              <Grid item>
+                <ButtonBase>
+                  <img alt="" src={trainer.profileImage} style={{ height: '300px' }} />
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid container>
+                  <Grid item xs>
+                    <Typography variant="h5" sx={{ mr: '3rem', mb: '1rem' }}>
+                      {`${trainer.firstName}${' '}${trainer.lastName}`}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h5" sx={{ float: 'left' }}>
+                      Description
+                    </Typography>
+                    <Typography variant="body2" sx={{ textAlign: 'left', mt: '2.5rem', mr: '1rem' }}>
+                      {trainer.description}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ float: 'left', mt: '1rem' }}>
+                    Qualifications
+                  </Typography>
+                  <Typography variant="body2" sx={{ textAlign: 'left', mt: '3.5rem' }}>
+                    {trainer.qualifications}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ float: 'left', mt: '1rem' }}>
+                    Contacts
+                  </Typography>
+                  <Typography variant="body2" sx={{ textAlign: 'left', mt: '3.5rem' }}>
+                    {trainer.phoneNumber}
+                  </Typography>
+                  <Typography variant="body2" sx={{ textAlign: 'left' }}>
+                    {trainer.email}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Box>
     </Container>
   );
 };
