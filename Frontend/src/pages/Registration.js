910c54e5fe360754ca0ba2913e4df80502909743
JuozasNarkevicius <juoznark@gmail.com>
2022-04-14 17:42:29 +0300

    Frontend: added style to registration page

--- Frontend/src/pages/Registration.js
+++ Frontend/src/pages/Registration.js
@@ -1,11 +1,13 @@
 import {
-  Button, Container, TextField,
+  Button, Container, TextField, CssBaseline, Box,
 } from '@mui/material';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import { authenticationService } from '../services/AuthenticationService';
 import ROUTES from '../constants/Routes';
+import backgroundImage from '../assets/images/workoutEquipment.jpg';
+import COLORS from '../styles/colors';
 
 const registationFields = [
   { name: 'firstName', label: 'First Name', type: 'text' },
@@ -42,23 +44,58 @@ const Registration = () => {
   });
 
   return (
-    <Container sx={{ width: '20rem', mt: '7rem' }}>
-      <form onSubmit={formik.handleSubmit}>
-        {registationFields.map((r) => (
-          <TextField
-            sx={{ m: '10px' }}
-            key={r.name}
-            name={r.name}
-            label={r.label}
-            type={r.type}
-            value={formik.values[r.name]}
-            onChange={formik.handleChange}
-            error={formik.touched[r.name] && Boolean(formik.errors[r.name])}
-            helperText={formik.touched[r.name] && formik.errors[r.name]}
-          />
-        ))}
-        <Button sx={{ margin: '10px' }} variant="contained" size="large" type="submit">Register</Button>
-      </form>
+    <Container sx={{
+      minWidth: '100%',
+      minHeight: '93vh',
+      paddingTop: '5rem',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      overflow: 'hidden',
+    }}
+    >
+      <CssBaseline />
+      <Box sx={{
+        display: 'flex', justifyContent: 'center', marginTop: '9vh',
+      }}
+      >
+        <form onSubmit={formik.handleSubmit} style={{ maxWidth: '20rem' }}>
+          {registationFields.map((r) => (
+            <TextField
+              InputLabelProps={{
+                style: { color: '#fff' },
+              }}
+              variant="filled"
+              sx={{
+                m: '10px', backgroundColor: '#2d2b2b', color: 'white',
+              }}
+              key={r.name}
+              name={r.name}
+              label={r.label}
+              type={r.type}
+              value={formik.values[r.name]}
+              onChange={formik.handleChange}
+              error={formik.touched[r.name] && Boolean(formik.errors[r.name])}
+              helperText={formik.touched[r.name] && formik.errors[r.name]}
+            />
+          ))}
+          <Button
+            sx={{
+              margin: '10px',
+              '&:hover': { backgroundColor: COLORS.SECONDARY_HOVER },
+              width: '8rem',
+            }}
+            variant="contained"
+            color="secondary"
+            type="submit"
+            size="large"
+          >
+            Register
+
+          </Button>
+        </form>
+      </Box>
     </Container>
   );
 };
