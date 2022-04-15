940892f370b64e37612c5cb64598a8203d0a8a2d
JuozasNarkevicius <juoznark@gmail.com>
2022-04-16 00:31:51 +0300

    Frontend: added design to modal in public program view

--- Frontend/src/components/dataDisplay/ExerciseDetails.js
+++ Frontend/src/components/dataDisplay/ExerciseDetails.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import {
   Typography, Card, IconButton, Icon,
 } from '@mui/material';
@@ -7,6 +6,7 @@ import PropTypes from 'prop-types';
 import YouTube from 'react-youtube';
 import exitIcon from '../../assets/icons/x.svg';
 import videoService from '../../services/genericServices/video';
+import COLORS from '../../styles/colors';
 
 const opts = {
   height: '390',
@@ -17,7 +17,13 @@ const ExerciseDetails = ({ exercise, setIsBackdropOpen }) => {
   const [videoEvent, setVideoEvent] = useState();
 
   return (
-    <Card sx={{ minHeight: '30rem', minWidth: '60rem', maxWidth: '60rem' }}>
+    <Card sx={{
+      minHeight: '30rem',
+      minWidth: '60rem',
+      maxWidth: '60rem',
+      backgroundColor: COLORS.ITEM,
+    }}
+    >
       <IconButton
         sx={{ float: 'right', m: 1 }}
         title="Exit"
@@ -31,12 +37,12 @@ const ExerciseDetails = ({ exercise, setIsBackdropOpen }) => {
         </Icon>
       </IconButton>
       <Typography variant="h5" sx={{ mt: '1rem' }}>{exercise.name}</Typography>
-      <Card sx={{ m: '2rem', p: '2rem' }}>
+      <Card sx={{ m: '2rem', p: '2rem', backgroundColor: COLORS.SUB_ITEM }}>
         {exercise.description
           ? <Typography>{exercise.description}</Typography>
           : <Typography>No description added</Typography>}
       </Card>
-      <Card sx={{ m: '2rem', p: '1rem' }}>
+      <Card sx={{ m: '2rem', p: '1rem', backgroundColor: COLORS.SUB_ITEM }}>
         {exercise.videoUrl ? (
           <YouTube
             videoUrl={exercise.videoUrl
