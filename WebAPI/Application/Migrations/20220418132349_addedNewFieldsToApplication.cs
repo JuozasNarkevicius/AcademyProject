1d0465c1e64df68daba6075a04a261b4f56e88f1
JuozasNarkevicius <juoznark@gmail.com>
2022-04-18 16:29:59 +0300

    Backend: added new fields to trainer application

---
+++ WebAPI/Application/Migrations/20220418132349_addedNewFieldsToApplication.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    public partial class addedNewFieldsToApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Specializations",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkExperience",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Specializations",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "WorkExperience",
+                table: "Applications");
+        }
+    }
+}
--- WebAPI/WebAPI/DTO's/CreateTrainerApplicationDTO.cs
+++ WebAPI/WebAPI/DTO's/CreateTrainerApplicationDTO.cs
@@ -8,6 +8,8 @@
         public string PhoneNumber { get; set; }
         public string Description { get; set; }
         public string Qualifications { get; set; }
+        public string Specializations { get; set; }
+        public string WorkExperience { get; set; }
         public string ImageId { get; set; }
         public long UserId { get; set; }
     }
