5c50d3d897f03d6a376db6b13089361c40cc2446
JuozasNarkevicius <juoznark@gmail.com>
2022-04-22 19:53:53 +0300

    Frontend: fixed not all fields being cleared when deleting trainer application and video not showing bug

--- Frontend/src/services/genericServices/video.js
+++ Frontend/src/services/genericServices/video.js
@@ -1,4 +1,11 @@
-const getIdFromUrl = (url) => url.substring(url.indexOf('=') + 1, url.indexOf('&'));
+const isPlaylistUrl = (url) => {
+  if (url.includes('&')) {
+    return url.indexOf('&');
+  }
+  return url.length;
+};
+
+const getIdFromUrl = (url) => url.substring(url.indexOf('=') + 1, isPlaylistUrl(url));
 
 const videoService = {
   getIdFromUrl,
